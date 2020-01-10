ad3808ab197180789864797f9484eab4aa06dd01
xiaoyu1998 <1634054493@qq.com>
2020-01-10 16:26:30 +0800

    wasm:update hello

--- examples/hello/src/hello.cpp
+++ examples/hello/src/hello.cpp
@@ -4,24 +4,6 @@
 
 ACTION hello::hi( name nm ) {
    print_f("Name : %\n", nm);
-
-   string h("hello");
-
-   wasm::checksum160 hash160 = wasm::sha1((const char*)h.data(), h.size());
-   printhex((const void*)&hash160, 20);
-   print("\n");
-
-   wasm::checksum256 hash256 = wasm::sha256((const char*)h.data(), h.size());
-   printhex((const void*)&hash256, 32);
-   print("\n");
-
-   wasm::checksum512 hash512 = wasm::sha512((const char*)h.data(), h.size());
-   printhex((const void*)&hash512, 64);
-   print("\n");
-
-   wasm::checksum160 ripemd160 = wasm::ripemd160((const char*)h.data(), h.size());
-   printhex((const void*)&ripemd160, 20);
-
 }
 
 ACTION hello::check( name nm ) {
